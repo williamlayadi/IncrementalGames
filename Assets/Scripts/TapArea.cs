4f8f9506ef150aac00c549f4e4198791336f2560
williamlayadi <layadi.william123@gmail.com>
2021-11-09 21:11:55 +0700

    Initial commit

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+    public AudioClip clip;
+    public AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        audioSource.PlayOneShot(clip);
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
